a6fc4ed8a2ad731a83510a64f6300341896e31a0
GriffithTx <390239178@qq.com>
2025-12-18 22:11:06 +0800

    后台配置小程序菜单栏显示

--- application/extra/site.php
+++ application/extra/site.php
@@ -20,7 +20,7 @@ return array (
     'article' => 'Article',
     'test' => 'Test',
   ),
-  'configgroup' => 
+  'configgroup' =>
   array (
     'basic' => 'Basic',
     'email' => 'Email',
@@ -28,6 +28,7 @@ return array (
     'user' => 'User',
     'example' => 'Example',
     'invite' => 'Invite',
+    'mp' => 'Miniprogram',
   ),
   'mail_type' => '1',
   'mail_smtp_host' => 'smtp.qq.com',
