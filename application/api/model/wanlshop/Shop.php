68788a77073571ea89b3662fbbfb5fa3d345f20d
Griffith <390239178@qq.com>
2026-01-05 21:44:52 +0800

    商家接口扩展 特定分类信息 惠选 优选

--- application/api/model/wanlshop/Shop.php
+++ application/api/model/wanlshop/Shop.php
@@ -19,7 +19,8 @@ class Shop extends Model
 	
 	// 追加属性
 	protected $append = [
-		'find_user'
+		'find_user',
+		'category_tags'
 	];
 
 	// 获取店铺 发现号
@@ -60,5 +61,58 @@ class Shop extends Model
 		   ];
 		}
 		return $service;
+	}
+
+	/**
+	 * 获取店铺商品分类标签
+	 * 检查店铺商品所属分类名称是否包含"惠选"或"精选"关键字
+	 * @param mixed $value
+	 * @param array $data
+	 * @return array 返回标签数组 ['huixuan' => bool, 'jingxuan' => bool]
+	 */
+	public function getCategoryTagsAttr($value, $data)
+	{
+		$tags = [
+			'huixuan' => false,
+			'jingxuan' => false
+		];
+
+		if (!isset($data['id'])) {
+			return $tags;
+		}
+
+		// 查询该店铺所有商品的分类ID
+		$categoryIds = \think\Db::name('wanlshop_goods')
+			->where('shop_id', $data['id'])
+			->where('category_id', '>', 0)
+			->column('category_id');
+
+		if (empty($categoryIds)) {
+			return $tags;
+		}
+
+		// 去重
+		$categoryIds = array_unique($categoryIds);
+
+		// 查询这些分类的名称
+		$categoryNames = \think\Db::name('wanlshop_category')
+			->whereIn('id', $categoryIds)
+			->column('name');
+
+		// 检查是否包含惠选或精选关键字
+		foreach ($categoryNames as $name) {
+			if (strpos($name, '惠选') !== false) {
+				$tags['huixuan'] = true;
+			}
+			if (strpos($name, '精选') !== false) {
+				$tags['jingxuan'] = true;
+			}
+			// 如果两个标签都已找到，提前退出
+			if ($tags['huixuan'] && $tags['jingxuan']) {
+				break;
+			}
+		}
+
+		return $tags;
 	}
 }
